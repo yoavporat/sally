ab4d383e9324461d8443d44b04b39ca0294805e4
Yoav Porat <yp.yoavporat@gmail.com>
2022-02-19 17:42:17 +0200

    get data from airtable

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'auto',
+    htmlWhitespaceSensitivity: 'css',
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    proseWrap: 'preserve',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'none',
+    useTabs: false
+};
